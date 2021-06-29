832527c2ef20d6d9ccc731d34e6f20a59d0b6ac0
Zhora D <halaulilau@gmail.com>
2021-06-29 13:01:13 +0600

    withoutCustom slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,24 @@
 export const Select = () => {
+    const headers = document.querySelectorAll('.select-header');
+    const items = document.querySelectorAll('.select-item');
 
+    const selectHeader = function () {
+        this.parentElement.classList.toggle("is-active")
+    };
+
+    const selectItem = function () {
+        const text = this.innerText;
+        const select = this.closest('.select');
+        const currentText = select.querySelector('.select-current-item');
+        currentText.innerText = text;
+        select.classList.remove('is-active');
+    };
+
+    items.forEach((item) => {
+        item.addEventListener('click', selectItem);
+    })
+
+    headers.forEach((header) => {
+        header.addEventListener('click', selectHeader)
+    });
 };
