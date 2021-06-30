5242d4d50fb6fb549099c4165295f4f5281fc83e
Zhora D <halaulilau@gmail.com>
2021-06-30 12:45:42 +0600

    mobile. Touch on slider  doen't work

--- src/js/app.js
+++ src/js/app.js
@@ -7,20 +7,27 @@ export const BurrgerMenu = () => {
     })
 
 };
+
 export const Select = () => {
     const headers = document.querySelectorAll('.select-header');
     const items = document.querySelectorAll('.select-item');
 
     const selectHeader = function () {
-        this.parentElement.classList.toggle("is-active")
+        console.log(this)
+        this.parentElement.classList.toggle("is-active");
+        const icon = this.querySelector('.select-icon');
+        icon.classList.toggle('is-active');
     };
+
 
     const selectItem = function () {
         const text = this.innerText;
         const select = this.closest('.select');
         const currentText = select.querySelector('.select-current-item');
+
         currentText.innerText = text;
         select.classList.remove('is-active');
+        select.querySelector('.select-icon').classList.remove('is-active');
     };
 
     items.forEach((item) => {
@@ -38,15 +45,49 @@ export const Slider = () => {
     const progress = document.querySelector('.progress')
     const knob = document.querySelector('.knob')
 
-    let levelsBox, knobBox, points, min, max
-    // knob.style.transform = "translateX(335.124px) rotate(-45deg); transition: all 0.5s ease 0s;";
-    // progress.style.transform = "all 0.5s ease 0s; width: 345.6px;"
-    // knob.style.transform = `translateX(${131}px) rotate(-45deg)`
-    // progress.style.width = 138 + 28 / 2 + 'px'
+    let levelsBox, knobBox, points, min = 0, max = 0;
+    // console.log(levels.querySelectorAll('.level')[0].getBoundingClientRect())
+    let allLevels = levels.querySelectorAll('.level');
+    let knobStartPosition = allLevels[0].getBoundingClientRect().width + allLevels[1].getBoundingClientRect().width - 10;
+
+    // Init values
+    window.addEventListener('resize', () => {
+        allLevels = levels.querySelectorAll('.level');
+        console.log('resize', allLevels[0].getBoundingClientRect().width);
+        knobStartPosition = allLevels[0].getBoundingClientRect().width + allLevels[1].getBoundingClientRect().width - 10;
+        knob.style.transform = `translateX(${knobStartPosition}px) rotate(-45deg)`;
+    });
+    knob.style.transform = `translateX(${knobStartPosition}px) rotate(-45deg)`;
+
+    let shift;
+    let X = 0;
+
+    knob.addEventListener('touchstart', function (e) {
+        e.preventDefault()
+
+        window.addEventListener('touchmove', dragging)
+        window.addEventListener('touchend', drop)
+
+        knob.style.transition = '0s'
+        progress.style.transition = '0s'
+
+        const knobBox = knob.getBoundingClientRect()
+        console.log(e)
+        // координата нажатия мыши относительно ручки
+        shift = e.changedTouches[0].clientX - knobBox.left;
+        console.log("shift", shift);
+
+        // при изменении ширины окна пересчитываем
+        // некоторые размеры для корректного отображения
+        window.onresize = init
+
+        init()
+    });
+
+
 
     knob.addEventListener('mousedown', function (e) {
-        // progress.style.width = 0;
-        // knob.style.left = e.x;
+
         e.preventDefault()
 
         window.addEventListener('mousemove', dragging)
@@ -57,42 +98,49 @@ export const Slider = () => {
 
         const knobBox = knob.getBoundingClientRect()
         // координата нажатия мыши относительно ручки
-        let shift = e.x - knobBox.left;
-        let X = 0;
+        shift = e.x - knobBox.left;
 
-        function dragging(e) {
-            let x = 0;
+        // при изменении ширины окна пересчитываем
+        // некоторые размеры для корректного отображения
+        window.onresize = init
 
-            // не даем выйти за границы
-            if (e.x < min) x = min
-            else if (e.x > max) x = max
-            else x = e.x
+        init()
+    });
 
-            X = x - shift - levelsBox.left
-            console.log(X);
-            console.log(knobBox.width);
-            knob.style.transform = `translateX(${X}px) rotate(-45deg)`
-            progress.style.width = X + knobBox.width / 2 + 'px'
-        }
 
-        function drop(e) {
-            // ближайшее число в массиве точек к текущей координате ручки
-            var closest = getClosest(X)
+    function dragging(e) {
+        var x = 0;
 
-            knob.style.transition = '0.5s'
-            progress.style.transition = '0.5s'
+        // не даем выйти за границы
+        if (e.x < min) x = min
+        else if (e.x > max) x = max
+        else x = e.x
 
-            // перемащаем к полученной ближайшей точке
-            knob.style.transform = `
-    	translateX(${closest - knobBox.width / 2.7}px)
-    	rotate(-45deg)
-     `
-            progress.style.width = closest + 'px'
+        X = x - shift - levelsBox.left
+        console.log("dragging", levelsBox.left, "shift", shift, "x", x)
+        console.log(X);
+        // console.log(knobBox.width);
+        knob.style.transform = `translateX(${X}px) rotate(-45deg)`
+        progress.style.width = X + knobBox.width / 2 + 'px'
+    }
 
-            window.removeEventListener('mousemove', dragging)
-            window.removeEventListener('mousemove', drop)
-        }
-    })
+    function drop(e) {
+        // ближайшее число в массиве точек к текущей координате ручки
+        var closest = getClosest(X)
+
+        knob.style.transition = '0.5s'
+        progress.style.transition = '0.5s'
+
+        // перемащаем к полученной ближайшей точке
+        knob.style.transform = `
+    translateX(${closest - knobBox.width / 2.7}px)
+    rotate(-45deg)
+ `
+        progress.style.width = closest + 'px'
+
+        window.removeEventListener('mousemove', dragging)
+        window.removeEventListener('mousemove', drop)
+    }
 
     function getClosest(v) {
         return points.reduce((prev, curr) =>
@@ -103,7 +151,7 @@ export const Slider = () => {
     function init() {
         levelsBox = levels.getBoundingClientRect()
         knobBox = knob.getBoundingClientRect()
-
+        console.log("init", levelsBox)
         /* ширина промежутков */
         points = [
             0,
@@ -118,9 +166,5 @@ export const Slider = () => {
         // progress.style.width = 0
     }
 
-    // при изменении ширины окна пересчитываем
-    // некоторые размеры для корректного отображения
-    window.onresize = init
 
-    init()
 }
